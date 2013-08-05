961c433e3f7df36780a4db16e0130257c46c4688
arnulfo4jc <arnulfo4jc@gmail.com>
2013-08-05 16:51:25 -0600

    set default configuration

---
+++ src/templates/artifacts/Controller.groovy
@@ -0,0 +1,11 @@
+@artifact.package@class @artifact.name@ {
+
+	static defaultAction = "actionHere"
+	static allowedMethods = [
+		list:"GET"
+	]
+
+    def index() {
+    	
+    }
+}
---
+++ src/templates/artifacts/DomainClass.groovy
@@ -0,0 +1,23 @@
+@artifact.package@class @artifact.name@ {
+
+
+
+	Date dateCReated
+	Date lastUpdated
+
+    static constraints = {
+
+    }
+
+    static namedQueries = {
+
+    }
+
+    static mapping = {
+    	version false
+    }
+
+    String toString() {
+
+    }
+}
